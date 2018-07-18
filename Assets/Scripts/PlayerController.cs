2f84634e056668032a7f903a16f0c2a2b613a8c2
唐卓 <tangz@yomob.com>
2018-07-18 15:56:28 +0800

    TzBallCreat

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Transform player;
+	Rigidbody2D playerRig;
+	public float upSpeed = 5;
+	private float moveSpeed = 0;
+	public float resetSpeed = 10;
+	public float gravityScale = 20;
+	public float commonScale = 2;
+	public float rightSpeed = 7;
+	public Transform mainCamera;
+	private bool isStart = false;
+	private bool isReset = true;
+	private Vector3 birthPosition;
+	// Use this for initialization
+	void Start () {
+		player = this.transform;
+		playerRig = player.GetComponent<Rigidbody2D> ();
+		birthPosition = player.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (mainCamera.position, new Vector3 (player.position.x + 1, mainCamera.position.y, mainCamera.position.z)) < 3) {
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				playerRig.gravityScale = gravityScale;
+				if (!isStart)
+					isStart = true;
+			}
+		}
+		mainCamera.position = Vector3.Lerp (mainCamera.position, new Vector3 (player.position.x + 1, mainCamera.position.y, mainCamera.position.z), Time.deltaTime*resetSpeed);
+		player.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+
+		if (!isReset) {
+			player.position = birthPosition;
+			if (Vector3.Distance (player.position, birthPosition) < 0.1) {
+				isReset = true;
+				isStart = false;
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		
+	}
+		
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.transform.tag == "step") {
+			playerRig.AddForce(Vector2.up * upSpeed);
+			playerRig.gravityScale = commonScale;
+			if (moveSpeed == 0 && isStart) {
+				moveSpeed = rightSpeed;
+			} 
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if (coll.transform.tag == "generateWall") {
+			StepGenerate.Instance.InstStep (5);
+		}
+		if (coll.transform.tag == "DeadLine") {
+			//player.position = birthPosition;
+			isReset = false;
+			moveSpeed = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/StepGenerate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepGenerate : MonoBehaviour {
+	private static StepGenerate _instance = null;
+	public GameObject step;
+	private GameObject firstStep;
+	private GameObject secondStep;
+	public GameObject generateWall;
+	public float stepDistance = 10;
+
+	void Awake(){
+		_instance = this;
+	}
+
+	public static StepGenerate Instance{
+		get{ return _instance;}
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (step == null || generateWall == null)
+			Debug.Log ("null gameobject");
+		firstStep = this.gameObject;
+		secondStep = Instantiate (step, GameObject.Find ("Steps").transform);
+		secondStep.transform.position = firstStep.transform.position + Vector3.right * stepDistance;
+		InstStep (5);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//生成台阶
+	public void InstStep(int number){
+		GameObject obj = step;
+		firstStep = secondStep;
+		for (int i = 0; i < number; i++) {
+			if (i == 0) {
+				Instantiate (generateWall, firstStep.transform.position, firstStep.transform.rotation);
+			}
+			secondStep = Instantiate (obj, GameObject.Find ("Steps").transform);
+			System.Random rd = new System.Random ();
+			int offset = rd.Next (-1, 2);
+			Vector3 offsetX = new Vector3 (offset, 0, 0);
+			secondStep.transform.position = new Vector3(firstStep.transform.position.x,0,0) + Vector3.right * stepDistance + Vector3.up * offset;
+			firstStep = secondStep;
+		}
+	}
+		
+}
